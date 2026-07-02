3d3598a7d6b76f345a7c6d57f19a72434c936773
aaryan-codev <aaryan.jadhav2.718@gmail.com>
2026-07-02 23:05:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two Equal Sum Subarrays/two-equal-sum-subarrays.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    bool canSplit(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        
+        vector<int>prefix(n);
+        prefix[0] = arr[0];
+        
+        for(int i=1; i<n; i++){
+            prefix[i] = prefix[i-1] + arr[i];
+        }
+        
+        vector<int>suffix(n);
+        suffix[n-1] = arr[n-1];
+        
+        for(int i=n-2; i >= 0; i--){
+            suffix[i] = suffix[i+1] + arr[i];
+        }
+        
+        for(int i=0; i<n-1; i++){
+            if(prefix[i] == suffix[i+1]){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
